b723231efe0054e70d4067487de1d42abef69bef
camposstefany08-gif <camposstefany08@gmail.com>
2025-08-15 04:35:38 -0600

    Actualizando App.js

--- App.js
+++ App.js
@@ -7,8 +7,15 @@ import SignUp from './screens/Sign-up';
 import MainTabs from './screens/MainTabs';
 import CulturalInterestsScreen from './screens/CulturalInterestsScreen';
 import SignIn from './screens/Sign-in';
-
+import Reservas from './screens/Reservas'; // ✅ Ruta correcta
+import CrearExperiencia from './screens/CrearExperiencia';
 import ExperienceDetail from './screens/ExperienceDetail'; // Ya lo tienes
+import Probar from './screens/Probar'; //ruta solo para probar otras pantallas
+import Capacitaciones from './screens/Capacitaciones'; // Nombre exacto del archivo
+import reservacion from './screens/reservacion';
+import chat from './screens/chat';
+
+
 
 const Stack = createStackNavigator();
 
@@ -21,6 +28,16 @@ export default function App() {
         <Stack.Screen name="SignIn" component={SignIn} />
         <Stack.Screen name="MainTabs" component={MainTabs} />
         <Stack.Screen name="CulturalInterests" component={CulturalInterestsScreen} />
+        <Stack.Screen name="chat" component={chat} />
+
+    
+  {/* otras pantallas */}
+  
+  <Stack.Screen name="Reservas" component={Reservas} />
+  <Stack.Screen name="CrearExperiencia" component={CrearExperiencia} />
+  <Stack.Screen name="Capacitaciones" component={Capacitaciones} />
+    <Stack.Screen name="Probar" component={Probar} />
+    <Stack.Screen name="reservacion" component={reservacion} />
 
         {/* ✅ Esta línea es clave */}
         <Stack.Screen name="ExperienceDetail" component={ExperienceDetail} />
